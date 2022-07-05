777113fb4dfe4568744221ed526f43a9f77be7e4
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-05 10:37:13 -0300

    right database connection, and ODM

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://breno:<Breno@157751>@cluster0.kqjil0s.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
