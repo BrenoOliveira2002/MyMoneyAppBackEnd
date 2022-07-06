d8f80ed6f6b37ef51996d7f28c3402aad1de6c96
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-06 08:31:54 -0300

    getting the number of records

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,20 @@
-const BillingCycle = require ("./billingCycle")
+const BillingCycle = require("./billingCycle");
 
-BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.methods(["get", "post", "put", "delete"]);
+BillingCycle.updateOptions({ new: true, runValidators: true });
+BillingCycle.route('count', (req,res,next) => {
+
+    BillingCycle.count((error, value) =>  {
+        if(error) {
+
+            res.status(500).json({errors: [error]})
+        }else {
+
+            res.json(value)
+        }
+    })
+})
+
 
 BillingCycle.route('get', (req, res, next) => {
 
@@ -21,4 +34,4 @@ BillingCycle.route('get', (req, res, next) => {
 
 })
 
-module.exports = BillingCycle
+module.exports = BillingCycle;
--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,21 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://breno:<Breno@157751>@cluster0.kqjil0s.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://breno:<password>@cluster0.kqjil0s.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' e obrigatorio."
+
+mongoose.Error.messages.Number.min =
+"O '{VALUE}'informado e menor que o limite minimo de '{MIN}'."
+mongoose.Error.messages.Number.max =
+"O '{VALUE}'informado e maior que o limite maximo de '{MAX}'."
+
+mongoose.Error.messages.String.enum =
+ "'{VALUE}' Nao e valido para o atributo '{PATH}' "
+
