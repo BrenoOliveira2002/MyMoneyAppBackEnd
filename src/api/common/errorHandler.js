a004e23f12dd0bd8195c1678b550a7acbe3e2609
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-06 09:07:24 -0300

    handling error messages

---
+++ src/api/common/errorHandler.js
@@ -0,0 +1,24 @@
+const _ = require('lodash') 
+const nodeRestful = require('node-restful')
+
+module.exports = (req, res, next) => {
+
+    const bundle = res.locals.bundle 
+
+    if(bundle.errors) {
+
+        const errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    } else {
+
+        next()
+    }
+}
+
+const parseErrors = (nodeRestfulErros) => {
+
+    const errors = []
+
+    _.forIn(nodeRestfulErrors, error => error.push(error.message))
+    return errors 
+}
